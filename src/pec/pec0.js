e8164f991bb07d5219d1aa453b21aec76c5e7ba0
David Rayo Torres <drayo@uoc.edu>
2022-02-27 12:07:50 +0100

    Pŕáctica finalizada

--- src/pec/pec0.js
+++ src/pec/pec0.js
@@ -24,4 +24,13 @@ export function sum2(array) {
     return array;
 }
 
-export function fibonacci(n) { }
+//Ejercicio 3: fibonacci
+export function fibonacci(n) {
+    if(n===0){
+        return 0;
+    } else if(n ===1){
+        return 1;
+    } else{
+        return (fibonacci(n-1)+fibonacci(n-2));
+    }
+ }
